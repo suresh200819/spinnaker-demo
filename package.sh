c8368f6b8fa2e7eee4c21af243af7f3116153b43
suresh200819 <79845886+suresh200819@users.noreply.github.com>
2021-03-05 16:41:33 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-New Jenkins Environment ......
 # packaging script for spinnaker-demo, puts a flat file
 # release into a deb package for artifacting and packing via
 # jenkins/packer/spinnaker
